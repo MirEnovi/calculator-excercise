d6ad897ad41b0c320ceb815557c62c13b70e5b87
MirEnovi <garciaivonne11@gmail.com>
2019-02-07 15:14:30 -0600

    add index console log

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const {
   multiply,
   divide
 } = require('./src/calculator.js');
+
+console.log(sum(4,5)); //9
+console.log(substract(4,5));  //-1
+console.log(multiply(4,5)); //20
+console.log(divide(4,5)); //.8
